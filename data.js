16dac3e3161b02642ab3fde6bd6b7c27f85ea0ec
Hein Swan Htet <hshscientist@gmail.com>
2024-03-10 18:26:40 +0630

    Create data.js and add sample emotions

---
+++ data.js
@@ -0,0 +1,25 @@
+const emotions = [
+    {
+        id: 2701,
+        type: '😊',
+        description: 'happy'
+    },
+
+    {
+        id: 2702,
+        type: '😢',
+        description: 'sad'
+    },
+
+    {
+        id: 2703,
+        type: '😡',
+        description: 'angry'
+    },
+
+    {
+        id: 2704,
+        type: '🤭',
+        description: 'super shy'
+    }
+]
