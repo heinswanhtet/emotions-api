35ddb2cc93610a03ab00b9cfcd46cb0ea050f56f
Hein Swan Htet <hshscientist@gmail.com>
2024-03-11 10:26:31 +0630

    Create GET method for api call

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const emotions = require('./routes/emotions')
 
 const app = express()
+
+app.use('/api/v1/emotions', emotions)
 
 app.listen('5001', () => {
     console.log('Server is listening on port 5001...')
